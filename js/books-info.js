1cb71eab9ff47bc299eb4944f04046cd46eedee0
BushraAzmatHussain <bushrahussain407@gmail.com>
2021-09-02 22:02:55 +0600

    updated url

--- js/books-info.js
+++ js/books-info.js
@@ -4,7 +4,7 @@ const searchBook = () => {
     console.log(searchText);
 
     searchFeild.value = '';
-    const url =` http://openlibrary.org/search.json?q=${searchText}`;
+    const url =` https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then (data => foundBooksNumber(data.docs, data.numFound));
